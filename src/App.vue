29b75346511bae8bf3af3c4685852f341d0268a2
Iman Setya A <iman.setya@batmandiri.com>
2021-01-19 15:18:45 +0700

    fix: fix query builder layout

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@
   </div>
 </template>
 <script>
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 export default {
-  name: 'App'
-}
+  name: "App",
+};
 </script>
---
+++ src/pages/perusahaan/MainPerusahaan.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-page> <vue-query-builder :rules="rules"></vue-query-builder> </q-page>
+</template>
+
+<script>
+import VueQueryBuilder from "vue-query-builder";
+export default {
+  name: "PerusdahaanMain",
+  components: { VueQueryBuilder },
+  data() {
+    return {
+      left: false,
+      rules: [
+        {
+          type: "text",
+          id: "vegetable",
+          label: "Vegetable",
+          operators: ["==", ">", "<"],
+        },
+        {
+          type: "select",
+          id: "fruit",
+          label: "Fruit",
+          choices: [
+            { label: "Apple", value: "apple" },
+            { label: "Banana", value: "banana" },
+          ],
+        },
+      ],
+    };
+  },
+};
+</script>
