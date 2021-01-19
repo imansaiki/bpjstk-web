ffb44310d2a102447df7ffc197c69dac2ea8319f
Iman Setya A <iman.setya@batmandiri.com>
2021-01-19 19:24:05 +0700

    fix: get q-card workaround

--- src/components/QuasarGroup.vue
+++ src/components/QuasarGroup.vue
@@ -1,39 +1,82 @@
 <template>
-  <!-- eslint-disable vue/no-v-html -->
-  <div>
-    <div class="row">
-      <div class="col">
-        <q-select
+  <div class="shadow-1" :class="'depth-' + depth.toString()">
+    <div
+      class="vqb-group-heading rounded-t py-3 px-5 bg-gray-300 border-b border-gray-400"
+    >
+      <div class="match-type-container form-inline">
+        <label class="mr-2" for="vqb-match-type">{{ labels.matchType }}</label>
+
+        <select
+          id="vqb-match-type"
           v-model="query.logicalOperator"
-          :options="labels.matchTypes"
-          :label="labels.matchType"
-        />
-      </div>
-      <div class="col">
-        <q-select v-model="selectedRule" :options="rules" label="Column" />
-      </div>
-      <div class="col self-end">
-        <q-btn color="primary" :label="labels.addRule" @click="addRule" />
+          class="inline-block w-auto mr-2 h-10 px-1 py-2 leading-normal gray-500 border border-gray-300 rounded"
+        >
+          <option
+            v-for="label in labels.matchTypes"
+            :key="label.id"
+            :value="label.id"
+          >
+            {{ label.label }}
+          </option>
+        </select>
+
+        <button
+          v-if="depth > 1"
+          type="button"
+          class="close ml-auto"
+          @click="remove"
+          v-html="labels.removeGroup"
+        ></button>
       </div>
     </div>
-    <div class="row">
-      <div class="col">
-        <q-btn color="primary" :label="labels.addGroup" @click="addGroup" />
+
+    <div class="vqb-group-body flex-auto p-5 mb-px">
+      <div class="mb-5 flex flex-wrap items-center">
+        <div class="form-group flex items-center flex-none">
+          <select
+            v-model="selectedRule"
+            class="inline-block w-auto mr-2 h-10 px-1 py-2 leading-normal gray-500 border border-gray-300 rounded"
+          >
+            <option v-for="rule in rules" :key="rule.id" :value="rule">
+              {{ rule.label }}
+            </option>
+          </select>
+
+          <button
+            type="button"
+            class="text-white px-3 py-2 rounded bg-gray-600 mr-2"
+            @click="addRule"
+          >
+            {{ labels.addRule }}
+          </button>
+
+          <button
+            v-if="depth < maxDepth"
+            type="button"
+            class="text-white px-3 py-2 rounded bg-gray-600"
+            @click="addGroup"
+          >
+            {{ labels.addGroup }}
+          </button>
+        </div>
       </div>
+
+      <query-builder-children v-bind="$props" />
     </div>
-    <query-builder-children v-bind="$props" />
   </div>
 </template>
 
 <script>
 import QueryBuilderGroup from "vue-query-builder/dist/group/QueryBuilderGroup.umd.js";
 import QueryBuilderRule from "./QuasarRule.vue";
+
 export default {
   name: "QueryBuilderGroup",
+
   components: {
-    // eslint-disable-next-line vue/no-unused-components
     QueryBuilderRule: QueryBuilderRule,
   },
+
   extends: QueryBuilderGroup,
 };
 </script>
--- src/components/QuasarRule.vue
+++ src/components/QuasarRule.vue
@@ -1,14 +1,14 @@
 <template>
   <!-- eslint-disable vue/no-v-html -->
-  <div class="vqb-rule card">
-    <div class="form-inline">
+  <div class="vqb-rule bg-gray-100 rounded border border-gray-300 mb-3 p-3">
+    <div class="flex flex-wrap items-center">
       <label class="mr-5">{{ rule.label }}</label>
 
       <!-- List of operands (optional) -->
       <select
         v-if="typeof rule.operands !== 'undefined'"
         v-model="query.operand"
-        class="form-control mr-2"
+        class="inline-block w-auto h-10 px-1 py-2 leading-normal gray-500 border border-gray-300 rounded mr-2"
       >
         <option v-for="operand in rule.operands" :key="operand">
           {{ operand }}
@@ -21,7 +21,7 @@
           typeof rule.operators !== 'undefined' && rule.operators.length > 1
         "
         v-model="query.operator"
-        class="form-control mr-2"
+        class="inline-block w-auto h-10 px-1 py-2 leading-normal gray-500 border border-gray-300 rounded mr-2"
       >
         <option
           v-for="operator in rule.operators"
@@ -36,69 +36,17 @@
       <input
         v-if="rule.inputType === 'text'"
         v-model="query.value"
-        class="form-control"
+        class="inline-block w-auto h-10 px-1 py-2 leading-normal gray-500 border border-gray-300 rounded"
         type="text"
         :placeholder="labels.textInputPlaceholder"
       />
-
-      <!-- Basic number input -->
-      <input
-        v-if="rule.inputType === 'number'"
-        v-model="query.value"
-        class="form-control"
-        type="number"
-      />
-
-      <!-- Datepicker -->
-      <input
-        v-if="rule.inputType === 'date'"
-        v-model="query.value"
-        class="form-control"
-        type="date"
-      />
-
-      <!-- Custom component input -->
-      <div v-if="isCustomComponent" class="vqb-custom-component-wrap">
-        <component
-          :is="rule.component"
-          :value="query.value"
-          @input="updateQuery"
-        />
-      </div>
-
-      <!-- Checkbox input -->
-      <template v-if="rule.inputType === 'checkbox'">
-        <div
-          v-for="choice in rule.choices"
-          :key="choice.value"
-          class="form-check form-check-inline"
-        >
-          <input
-            :id="
-              'depth' + depth + '-' + rule.id + '-' + index + '-' + choice.value
-            "
-            v-model="query.value"
-            type="checkbox"
-            :value="choice.value"
-            class="form-check-input"
-          />
-          <label
-            class="form-check-label"
-            :for="
-              'depth' + depth + '-' + rule.id + '-' + index + '-' + choice.value
-            "
-          >
-            {{ choice.label }}
-          </label>
-        </div>
-      </template>
 
       <!-- Radio input -->
       <template v-if="rule.inputType === 'radio'">
         <div
           v-for="choice in rule.choices"
           :key="choice.value"
-          class="form-check form-check-inline"
+          class="flex items-center justify-center w-auto mr-3"
         >
           <input
             :id="
@@ -108,56 +56,17 @@
             :name="'depth' + depth + '-' + rule.id + '-' + index"
             type="radio"
             :value="choice.value"
-            class="form-check-input"
+            class="flex-shrink-0 mr-1 ml-0 mt-0 form-check-input"
           />
           <label
-            class="form-check-label"
+            class="flex items-center justify-center"
             :for="
               'depth' + depth + '-' + rule.id + '-' + index + '-' + choice.value
             "
+            >{{ choice.label }}</label
           >
-            {{ choice.label }}
-          </label>
         </div>
       </template>
-
-      <!-- Select without groups -->
-      <select
-        v-if="rule.inputType === 'select' && !hasOptionGroups"
-        v-model="query.value"
-        class="form-control"
-        :multiple="rule.type === 'multi-select'"
-      >
-        <option
-          v-for="option in selectOptions"
-          :key="option.value"
-          :value="option.value"
-        >
-          {{ option.label }}
-        </option>
-      </select>
-
-      <!-- Select with groups -->
-      <select
-        v-if="rule.inputType === 'select' && hasOptionGroups"
-        v-model="query.value"
-        class="form-control"
-        :multiple="rule.type === 'multi-select'"
-      >
-        <optgroup
-          v-for="(option, option_key) in selectOptions"
-          :key="option_key"
-          :label="option_key"
-        >
-          <option
-            v-for="sub_option in option"
-            :key="sub_option.value"
-            :value="sub_option.value"
-          >
-            {{ sub_option.label }}
-          </option>
-        </optgroup>
-      </select>
 
       <!-- Remove rule button -->
       <button
@@ -172,6 +81,7 @@
 
 <script>
 import QueryBuilderRule from "vue-query-builder/dist/rule/QueryBuilderRule.umd.js";
+
 export default {
   extends: QueryBuilderRule,
 };
