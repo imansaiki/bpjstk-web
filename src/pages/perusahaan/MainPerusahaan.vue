603e29f26c02f51ec3a86f45e5a0d5d282c7c650
Iman Setya A <iman.setya@batmandiri.com>
2021-01-19 15:47:53 +0700

    fix: create base layout for List Perusahaan

--- src/pages/perusahaan/MainPerusahaan.vue
+++ src/pages/perusahaan/MainPerusahaan.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-page>
+  <q-page padding>
     <q-list bordered class="rounded-borders">
       <q-expansion-item
         expand-separator
@@ -14,6 +14,15 @@
         </q-card>
       </q-expansion-item>
     </q-list>
+    <div class="q-pt-md">
+      <q-table
+        title="Treats"
+        dense
+        :data="data"
+        :columns="columns"
+        row-key="name"
+      />
+    </div>
   </q-page>
 </template>
 
@@ -28,18 +37,54 @@ export default {
       rules: [
         {
           type: "text",
-          id: "vegetable",
-          label: "Vegetable",
+          id: "perusahaan",
+          label: "Perusahaan",
           operators: ["==", ">", "<"],
         },
         {
           type: "select",
-          id: "fruit",
-          label: "Fruit",
+          id: "pembina",
+          label: "Pembina",
           choices: [
-            { label: "Apple", value: "apple" },
-            { label: "Banana", value: "banana" },
+            { label: "Sukri Alnawawi", value: "Sukri Alnawawi" },
+            { label: "Badrun Jawaludin", value: "badrun Jawaludin" },
           ],
+        },
+      ],
+      columns: [
+        {
+          name: "name",
+          required: true,
+          label: "Dessert (100g serving)",
+          align: "left",
+          field: (row) => row.name,
+          format: (val) => `${val}`,
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        { name: "fat", label: "Fat (g)", field: "fat", sortable: true },
+        { name: "carbs", label: "Carbs (g)", field: "carbs" },
+        { name: "protein", label: "Protein (g)", field: "protein" },
+        { name: "sodium", label: "Sodium (mg)", field: "sodium" },
+        {
+          name: "calcium",
+          label: "Calcium (%)",
+          field: "calcium",
+          sortable: true,
+          sort: (a, b) => parseInt(a, 10) - parseInt(b, 10),
+        },
+        {
+          name: "iron",
+          label: "Iron (%)",
+          field: "iron",
+          sortable: true,
+          sort: (a, b) => parseInt(a, 10) - parseInt(b, 10),
         },
       ],
     };
